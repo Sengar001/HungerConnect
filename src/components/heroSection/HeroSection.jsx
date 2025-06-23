3fe6a6b18bedd1e3bee3b6d4835c2d656abab585
Sengar001 <abhisinghsengar001@gmail.com>
2025-06-23 10:25:53 +0530

    firebase setup completed

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react'
+import myContext from '../../context/data/myContext';
+
+
+function HeroSection() {
+    const context = useContext(myContext);
+    const { mode } = context;
+    return (
+        <section
+            style={{ background: mode === 'dark' ? 'rgb(30, 41, 59)' : '#30336b' }}>
+
+            {/* Hero Section  */}
+            <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+                {/* Main Content  */}
+                <main>
+                    <div className="text-center">
+                        <div className="mb-2">
+                            {/* Image  */}
+                            <div className="flex justify-center">
+                                <img 
+                                className='w-[600px] h-[100px]'
+                                src="https://i.imgur.com/8Zaotmf.png" alt="logo" 
+                                />
+                            </div>
+
+                            {/* Text  */}
+                            {/* <h1 className=' text-3xl text-white font-bold'>No Hunger</h1> */}
+                        </div>
+
+                        {/* Paragraph  */}
+                        <p
+                            style={{ color: mode === 'dark' ? 'white' : 'white' }}
+                            className="sm:text-3xl text-xl font-extralight sm:mx-auto ">
+                            Come together Food Provider and Distributor to Reduce Food Wastage
+                        </p>
+                    </div>
+
+                </main>
+            </div>
+        </section>
+    )
+}
+
+export default HeroSection
